9928fa559afa0248b52bad52c3ad7b14308c0121
eserranor98 <118926192+eserranor98@users.noreply.github.com>
2023-06-16 15:34:04 -0600

    initial commit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import requestIp from "request-ip";
 
 type Data = {
-  name: string
-}
+  message?: string;
+  ip?: any;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const ip = requestIp.getClientIp(req);
+  const message = "hello world";
+
+  res.status(200).json({ message, ip });
 }
